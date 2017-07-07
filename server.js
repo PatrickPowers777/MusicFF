1e8e51cc048a57656dcf39b36b8eb0ade0289cae
patrickpowers848@gmail.com <patrickpowers848@gmail.com>
2017-07-07 18:55:24 -0400

    update

--- server.js
+++ server.js
@@ -1,35 +1,62 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-// Get our API routes
-const api = require('./server/routes/api');
+// var appRoutes = require('./routes/app');
+// var messageRoutes = require('./routes/messages');
+// var userRoutes = require('./routes/user');
 
-const app = express();
+var app = express();
+mongoose.promise = Promise;
 
-// Parsers for POST data
+var db = process.env.MONGODB_URI || "mongodb://localhost/music-forward";
+
+mongoose.connect(db, function(error) {
+ 
+  if (error) {
+    console.log(error);
+  }
+
+  else {
+    console.log("mongoose connection is successful");
+  }
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
-// Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Set our api routes
-app.use('/api', api);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 
-// Catch all other routes and return the index file
+// app.use('/message', messageRoutes);
+// app.use('/user', userRoutes);
+// app.use('/', appRoutes);
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3030';
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    return res.render('index');
+});
+
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
 app.listen(port, () => console.log(`API running on localhost:${port}`));
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
-import { SignInComponent } from './sign-in/sign-in.component';
 import {HeaderComponent} from './header.component';
-import { SpotifyComponent } from './spotify/spotify.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent,
-    HeaderComponent,
-    SpotifyComponent
-  ],
+    HeaderComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
