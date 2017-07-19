994b1e09619a8b8440210fcc6e3628f29724852c
Patrick Powers <patrickpowers848@gmail.com>
2017-07-19 01:27:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ mongoose.connect(db, function(error) {
   }
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
